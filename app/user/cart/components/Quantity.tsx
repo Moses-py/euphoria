dd8a4ede30f578a64a46e4208c2279616d5adda3
Moses Chukwunekwu <dantereus1@gmail.com>
2023-08-08 20:49:21 +0100

    Minor fix

--- app/user/cart/components/Quantity.tsx
+++ app/user/cart/components/Quantity.tsx
@@ -7,18 +7,17 @@ const Quantity = ({ minusOnclick, plusOnclick, count }: QuantityProps) => {
   return (
     <div className="flex items-center justify-start xs:justify-end text-gray-1  border-gray-3 rounded-sm font-sans">
       <button
-        disabled={count < 1 && true}
-        className={`disabled:bg-gray-2 flex items-center justify-center w-8 h-8 text-lg text-white rounded-l-lg bg-primary hover:bg-purple-900`}
+        className={`disabled:bg-gray-2 flex items-center justify-center px-3 text-xl bg-gray-3 text-primary rounded-l-lg hover:bg-gray-2`}
         onClick={minusOnclick}
       >
         -
       </button>
-      <div className="flex items-center text-primary justify-center w-8 h-8 text-base font-light">
+      <div className="flex items-end text-primary justify-center px-3 text-base font-light">
         {count}
       </div>
       <button
         className={
-          "flex items-center justify-center w-8 h-8 text-lg text-white rounded-r-lg hover:bg-purple-900 bg-primary"
+          "flex items-center justify-center px-3 text-xl bg-gray-3 text-primary rounded-r-lg hover:bg-gray-2"
         }
         onClick={plusOnclick}
       >
